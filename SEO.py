92a5165984a62451f2a7da437024c18864e089fb
PotatoGambit <47349627+PotatoGambit@users.noreply.github.com>
2019-02-08 13:06:03 +0200

    V2

--- SEO.py
+++ SEO.py
@@ -7,9 +7,11 @@ Created on Thu Feb  7 17:06:50 2019
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import re
 
 url = input("Which page wuld you like to check? Enter Full URL: ")
 keyword = input("What is you seo keyword")
+keyword = keyword.casefold()
 
 try:
     html = urlopen(url)
@@ -18,11 +20,36 @@ except HTTPError as e:
     
 data = BeautifulSoup(html,"html.parser")
 
-def seo_title(keyword,data):
-    if keyword.casefold() in data.title.text.casefold():
-        status = "Found"
+def seo_title_found(keyword,data):
+    if data.title:
+        
+        if keyword in data.title.text.casefold():
+            status = "Found"
+        else:
+            status = "Keyword Not Found"
     else:
-        status = "Not Found"
+       status = "No title Found"
+       
     return status
 
-print(seo_title(keyword,data))
+#Search engine wont crawl over these words
+def seo_title_stop_words(data): 
+    words = 0
+    list_words =[]
+    if data.title:
+        with open('stopwords.txt','r') as f:
+            for line in f:
+                if re.search(r'\b'+line.rstrip('\n') + r'\b', data.title.text.casefold()):
+                    words += 1
+                    list_words.append(line.rstrip('\n'))
+        if words > 0 :
+            stop_words = "We Found {} stop words in your title. {}".format(words,list_words)
+        else:
+            stop_words = "We found no stop words in the title"
+    else:
+        stop_words = "We could not find a title"
+    return stop_words
+        
+
+print(seo_title_found(keyword,data))
+print(seo_title_stop_words(data))
